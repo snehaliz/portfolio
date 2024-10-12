ce9c460afddf068880d834b525c87cce837cd3f0
Sneha Shah <snehaeshah@gmail.com>
2024-10-12 09:06:57 -0700

    Lab 3 Steps 1-3

---
+++ global.js
@@ -0,0 +1,56 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// document.addEventListener('DOMContentLoaded',() => {
+//   const navLinks = $$("nav a");
+
+//   let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+//   if (currentLink) {
+//     currentLink.classList.add('current');
+//   }
+
+// }); 
+
+document.addEventListener('DOMContentLoaded',() => {
+  let pages = [
+    {url:'index.html',title:'Home'},
+    {url:'projects/index.html',title:'Projects'},
+    {url:'contact/index.html',title:'Contact'},
+    {url:'https://github.com/snehaliz/portfolio',title:'Profile'},
+    {url:'resume.html',title:'Resume'}
+  ];
+  
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
+  
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    a.classList.toggle(
+      'current',
+      a.host === location.host && a.pathname === location.pathname,
+    );
+
+    if (a.host !== location.host) {
+      a.target = "_blank";
+    }
+
+    nav.append(a);
+  }
+});
